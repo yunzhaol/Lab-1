5f5f271764e8c8cefd2fbb4e2bbbc1e12a1a9cc5
yunzhaoli <yunzhao.li@mail.utoronto.ca>
2025-05-17 01:14:35 -0400

    change for to while

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -5,7 +5,8 @@ class FizzBuzz {
 
     public static void main(String[] args) {
 
-        for (int i = 1; i < 100; i++) {
+        int i = 1;
+        while (i < 100) {
 
             // Find out which numbers divide i.
             boolean divisibleBy3 = i % 3 == 0;
@@ -29,6 +30,7 @@ class FizzBuzz {
                 System.out.println(i);
 
             }
+            i++;
         }
     }
 }
